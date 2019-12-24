22f7e39a850c414a7189bba6e28f04626153f38b
gaopenghh <1347210705@qq.com>
2019-12-24 16:33:59 +0800

    WaterMapper

---
+++ sx-iot/src/main/java/com/ruoyi/iot/mapper/WaterMapper.java
@@ -0,0 +1,46 @@
+package com.ruoyi.iot.mapper;
+import com.ruoyi.iot.domain.Water;
+
+import java.util.List;
+public interface WaterMapper {
+    /**
+     * 水质记录列表
+     *
+     * @return
+     */
+    List<Water> selectWaterList(Water water);
+
+    /**
+     * 根据wid删除数据
+     * @param wid
+     */
+    int deleteWaterByids(String[] wid);
+
+    /**
+     * 添加水质信息
+     * @param water
+     * @return
+     */
+    int insertWater(Water water);
+
+    /**
+     * 根据wid选择数据
+     * @param wid
+     * @return
+     */
+    Water selectBywid(String wid);
+
+    /**
+     * 保存用户修改
+     * @param water
+     * @return
+     */
+    int updateWater(Water water);
+
+    /**
+     * 删除水质信息
+     * @param wid
+     * @return
+     */
+    int deleteWaterByIds(String wid);
+}

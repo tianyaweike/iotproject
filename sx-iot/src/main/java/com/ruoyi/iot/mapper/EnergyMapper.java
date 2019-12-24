b45e5ad814ac88059115c420f9a84e9f757f8c23
gaopenghh <1347210705@qq.com>
2019-12-24 16:34:55 +0800

    EnergyMapper

---
+++ sx-iot/src/main/java/com/ruoyi/iot/mapper/EnergyMapper.java
@@ -0,0 +1,46 @@
+package com.ruoyi.iot.mapper;
+import com.ruoyi.iot.domain.Energy;
+
+import java.util.List;
+public interface EnergyMapper {
+    /**
+     * 能源管理记录列表
+     *
+     * @return
+     */
+    List<Energy> selectEnergyList(Energy energy);
+
+    /**
+     * 根据eid删除数据
+     * @param eid
+     */
+    int deleteEnergyByids(String[] eid);
+
+    /**
+     * 添加能源管理记录信息
+     * @param energy
+     * @return
+     */
+    int insertEnergy(Energy energy);
+
+    /**
+     * 根据eid选择数据
+     * @param eid
+     * @return
+     */
+    Energy selectByeid(String eid);
+
+    /**
+     * 保存用户修改
+     * @param energy
+     * @return
+     */
+    int updateEnergy(Energy energy);
+
+    /**
+     * 删除能源管理记录信息
+     * @param eid
+     * @return
+     */
+    int deleteEnergyByIds(String eid);
+}

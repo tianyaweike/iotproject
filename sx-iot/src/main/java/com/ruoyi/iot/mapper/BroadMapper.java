bfd6840dbd9fffa44af6b9ac3d085f504f1b7132
gaopenghh <1347210705@qq.com>
2019-12-24 16:32:03 +0800

    BroadMapper

---
+++ sx-iot/src/main/java/com/ruoyi/iot/mapper/BroadMapper.java
@@ -0,0 +1,46 @@
+package com.ruoyi.iot.mapper;
+import com.ruoyi.iot.domain.Broad;
+
+import java.util.List;
+public interface BroadMapper {
+    /**
+     * 广播记录列表
+     *
+     * @return
+     */
+    List<Broad> selectBroadList(Broad broad);
+
+    /**
+     * 根据broad_id删除数据
+     * @param broad_id
+     */
+    int deleteBroadByids(String[] broad_id);
+
+    /**
+     * 添加广播信息
+     * @param broad
+     * @return
+     */
+    int insertBroad(Broad broad);
+
+    /**
+     * 根据broad_id选择数据
+     * @param broad_id
+     * @return
+     */
+    Broad selectByshiid(String broad_id);
+
+    /**
+     * 保存用户修改
+     * @param broad
+     * @return
+     */
+    int updateBroad(Broad broad);
+
+    /**
+     * 删除广播信息
+     * @param broad_id
+     * @return
+     */
+    int deleteBroadByIds(String broad_id);
+}

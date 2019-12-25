cbeb5b62f23e2efce7bb6297f49da1b1c5573127
Rpurgatory <1943291256@qq.com>
2019-12-25 14:43:50 +0800

    能源管理和路灯Service层

---
+++ sx-iot/src/main/java/com/ruoyi/iot/service/EnergyService.java
@@ -0,0 +1,49 @@
+package com.ruoyi.iot.service;
+import com.ruoyi.iot.domain.Energy;
+
+import java.util.List;
+/**
+ * @program: EnergySys
+ * @description: 能源管理系统
+ * @author: 饶泽敏
+ * @create: 2019-12-25 14:20
+ **/
+public interface EnergyService {
+    /**
+     * 查询能源管理记录列表
+     *
+     * @return
+     */
+    public List<Energy> selectEnergyList(Energy energy);
+
+    /**
+     * 根据eid批量删除数据
+     * @param id
+     */
+    public int deleteEnergyByids(String eid);
+
+    /**
+     * 添加能源管理公告数据
+     * @param  Energy
+     * @return
+     */
+    public int insertEnergy(Energy energy);
+    /**
+     * 根据eid选择数据
+     * @param eid
+     * @return
+     */
+    public Energy selectByeid(String eid);
+    /**
+     * 保存用户修改
+     * @param Energy
+     * @return
+     */
+    public int updateEnergy(Energy energy);
+    /**
+     * 删除能源管理信息
+     * @param eid
+     * @return
+     */
+    public int deleteEnergyByIds(String eid);
+}
---
+++ sx-iot/src/main/java/com/ruoyi/iot/service/impl/LightServiceImpl.java
@@ -0,0 +1,89 @@
+package com.ruoyi.iot.service.impl;
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.enums.DataSourceType;
+import com.ruoyi.common.support.Convert;
+import com.ruoyi.iot.service.*;
+import com.ruoyi.iot.domain.Light;
+import com.ruoyi.iot.mapper.LightMapper;
+import com.ruoyi.iot.service.LightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @program:  LightSys
+ * @description: 路灯系统
+ * @author: 饶泽敏
+ * @create: 2019-12-25 14:40
+ **/
+@Service
+public class LightServiceImpl implements LightService {
+    @Autowired
+    private LightMapper lightMapper;
+
+    /**
+     * 查询路灯记录列表
+     *
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXVILLAGE)
+    public List<Light> selectLightList(Light light) {
+        return lightMapper.selectLightList(light);
+    }
+
+    /**
+     * 根据id批量删除数据
+     * @param id
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXVILLAGE)
+    public int deleteLightByids(String nid) {
+        return lightMapper.deleteLightByids(Convert.toStrArray(nid));
+    }
+
+    /**
+     * 添加路灯公告数据
+     * @param  Light
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXVILLAGE)
+    public int insertLight(Light light) {
+        return lightMapper.insertLight(light);
+    }
+
+    /**
+     * 根据nid选择数据
+     * @param nid
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXVILLAGE)
+    public Light selectBynid(String nid) {
+        return lightMapper.selectBynid(nid);
+    }
+
+    /**
+     * 删除路灯信息
+     * @param Light
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXVILLAGE)
+    public int deleteLightByIds(String nid) {
+        return lightMapper.deleteLightByIds(nid);
+    }
+
+    /**
+     * 保存用户修改
+     * @param Light
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXVILLAGE)
+    public int updateLight(Light light) {
+        return lightMapper.updateLight(light);
+    }
+}

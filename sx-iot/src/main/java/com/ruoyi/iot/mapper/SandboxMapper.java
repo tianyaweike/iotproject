bffb31fdda84408d4351002cef2387484ebe4c44
gaopenghh <1347210705@qq.com>
2019-12-26 15:01:12 +0800

    SandboxMapper

---
+++ sx-iot/src/main/java/com/ruoyi/iot/mapper/SandboxMapper.java
@@ -0,0 +1,46 @@
+package com.ruoyi.iot.mapper;
+import com.ruoyi.iot.domain.Sandbox;
+
+import java.util.List;
+public interface SandboxMapper {
+    /**
+     * 沙盘记录列表
+     *
+     * @return
+     */
+    List<Sandbox> selectSandboxList(Sandbox sandbox);
+
+    /**
+     * 根据switch_id删除数据
+     * @param switch_id
+     */
+    int deleteSandboxByids(String[] switch_id);
+
+    /**
+     * 添加沙盘信息
+     * @param sandbox
+     * @return
+     */
+    int insertSandbox(Sandbox sandbox);
+
+    /**
+     * 根据switch_id选择数据
+     * @param switch_id
+     * @return
+     */
+    Sandbox selectByswitch_id(String switch_id);
+
+    /**
+     * 保存用户修改
+     * @param sandbox
+     * @return
+     */
+    int updateSandbox(Sandbox sandbox);
+
+    /**
+     * 删除沙盘信息
+     * @param switch_id
+     * @return
+     */
+    int deleteSandboxByIds(String switch_id);
+}

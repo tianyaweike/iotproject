0efe15b506066ce0547913634f9c97ce2d291815
dwsbb <1135447852@qq.com>
2019-12-26 22:30:44 +0800

    MountainfloodController层

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/MountainfloodController.java
@@ -0,0 +1,97 @@
+package com.ruoyi.web.controller.iot;
+
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.base.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.page.TableDataInfo;
+import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.iot.domain.Mountainflood;
+import com.ruoyi.iot.service.MountainfloodService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/iot/mountainfloodinfo")
+public class MountainfloodController extends BaseController {
+
+    private String prefix = "iot/mountainfloodinfo";
+
+    @Autowired
+    private MountainfloodService MountainfloodService;
+
+    @RequiresPermissions("iot:mountainfloodinfo:view")
+    @GetMapping()
+    public String Mountainflood(){
+        return prefix+"/mountainflood";
+    }
+
+    /**
+     * MOUNTAINFLOOD列表
+     * @param mountainflood
+     * @return
+     */
+    //@RequiresPermissions("iot:mountainfloodinfo:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(MOUNTAINFLOOD mountainflood){
+        startPage();
+        List<Mountainflood> list = MountainfloodService.selectMountainfloodList(mountainflood);
+        return getDataTable(list);
+    }
+
+    @GetMapping("/add")
+    public String add(){
+        return prefix+"/add";
+    }
+
+    /**
+     * 新增MOUNTAINFLOOD信息
+     */
+    @RequiresPermissions("iot:mountainfloodinfo:add")
+    @Log(title = "MOUNTAINFLOOD信息", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(MOUNTAINFLOOD mountainflood)
+    {
+        return toAjax(MountainfloodService.insertmountainflood(mountainflood));
+    }
+    /**
+     * 修改MOUNTAINFLOOD信息
+     */
+    @GetMapping("/edit/{place_id}")
+    public String edit(@PathVariable("place_id") String place_id, ModelMap mmap)
+    {
+        MOUNTAINFLOOD mountainflood = MountainfloodService.selectByplace_id(place_id);
+        mmap.put("mountainflood", mountainflood);
+        return prefix + "/edit";
+    }
+    /**
+     * 修改保存MOUNTAINFLOOD信息
+     */
+    @RequiresPermissions("iot:mountainfloodinfo:edit")
+    @Log(title = "MOUNTAINFLOOD信息", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(MOUNTAINFLOOD mountainflood)
+    {
+        return toAjax(MountainfloodService.updateMountainflood(mountainflood));
+    }
+    /**
+     * 删除MOUNTAINFLOOD信息
+     */
+    @RequiresPermissions("iot:mountainfloodinfo:remove")
+    @Log(title = "删除MOUNTAINFLOOD信息", businessType = BusinessType.DELETE)
+
+    @PostMapping( "/remove/{place_id}")
+    @ResponseBody
+    public AjaxResult remove(@PathVariable("place_id") String place_id)
+    {
+        System.out.println("*******"+place_id);
+        return toAjax(MountainfloodService.deleteMountainfloodByids(place_id));
+    }
+}

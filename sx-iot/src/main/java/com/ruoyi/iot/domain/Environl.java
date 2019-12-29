cdb53cb30a602affce9a90ef32554f38d527aafa
dwsbb <1135447852@qq.com>
2019-12-29 13:23:29 +0800

    无功能实现

---
+++ sx-iot/src/main/java/com/ruoyi/iot/domain/Environl.java
@@ -0,0 +1,110 @@
+package com.ruoyi.iot.domain;
+
+public class Environl {
+    private String hid;
+
+    private String Ambient_temperature;
+
+    private String ambient_humidity;
+    private String wind_speed;
+    private String wind_direction;
+    private String Light_intensity;
+    private String atmospheric_pressure;
+    private String PM;
+    private String ppm;
+    private String Device_status;
+    @Override
+    public String toString() {
+        return "Environl{" +
+                "hid='" + hid + '\'' +
+                ", Ambient_temperature='" + Ambient_temperature + '\'' +
+                ", ambient_humidity='" + ambient_humidity + '\'' +
+                ", wind_speed='" + wind_speed + '\'' +
+                ", wind_direction='" + wind_direction + '\'' +
+                ", Light_intensity='" + Light_intensity + '\'' +
+                ", atmospheric_pressure='" + atmospheric_pressure + '\'' +
+                ", PM='" + PM + '\'' +
+                ", ppm='" + ppm + '\'' +
+                ", Device_status='" + Device_status + '\'' +
+                '}';
+    }
+
+    public Environl(String hid, String Ambient_temperature, String ambient_humidity, String wind_speed, String wind_direction, String light_intensity, String atmospheric_pressure, String PM, String ppm, String device_status) {
+            this.hid = hid;
+            Ambient_temperature = Ambient_temperature;
+            this.ambient_humidity = ambient_humidity;
+            this.wind_speed = wind_speed;
+            this.wind_direction = wind_direction;
+            Light_intensity = light_intensity;
+            this.atmospheric_pressure = atmospheric_pressure;
+            this.PM = PM;
+            this.ppm = ppm;
+            Device_status = device_status;
+        }
+    public Environl() {
+            super();
+        }
+
+        public String getHid() {
+            return hid;
+        }
+
+        public void setHid(String hid) {
+            this.hid = hid;
+        }
+
+        public String getAmbient_temperature() {
+            return Ambient_temperature;
+        }
+        public void setAmbient_temperature(String Ambient_temperature) {
+            Ambient_temperature = Ambient_temperature;
+        }
+        public String getAmbient_humidity() {
+            return ambient_humidity;
+        }
+        public void setAmbient_humidity(String ambient_humidity) {
+            this.ambient_humidity = ambient_humidity;
+        }
+        public String getWind_speed() {
+            return wind_speed;
+        }
+        public void setWind_speed(String wind_speed) {
+            this.wind_speed = wind_speed;
+        }
+        public String getWind_direction() {
+            return wind_direction;
+        }
+        public void setWind_direction(String wind_direction) {
+            this.wind_direction = wind_direction;
+        }
+        public String getLight_intensity() {
+            return Light_intensity;
+        }
+        public void setLight_intensity(String light_intensity) {
+            Light_intensity = light_intensity;
+        }
+        public String getAtmospheric() {
+            return atmospheric_pressure;
+        }
+        public void setAtmospheric(String atmospheric_pressure) {
+            this.atmospheric_pressure = atmospheric_pressure;
+        }
+        public String getPM() {
+            return PM;
+        }
+        public void setPM(String PM) {
+            this.PM = PM;
+        }
+        public String getPpm() {
+            return ppm;
+        }
+        public void setPpm(String ppm) {
+            this.ppm = ppm;
+        }
+        public String getDevice_status() {
+            return Device_status;
+        }
+        public void setDevice_status(String device_status) {
+            Device_status = device_status;
+        }
+    }
---
+++ sx-iot/src/main/java/com/ruoyi/iot/service/impl/EnvironlServiceImpl.java
@@ -0,0 +1,79 @@
+package com.ruoyi.iot.service.impl;
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.enums.DataSourceType;
+import com.ruoyi.common.support.Convert;
+import com.ruoyi.iot.service.*;
+import com.ruoyi.iot.domain.Environl;
+import com.ruoyi.iot.mapper.EnvironlMapper;
+import com.ruoyi.iot.service.EnvironlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class EnvironlServiceImpl  implements EnvironlService {
+    @Autowired
+    private EnvironlMapper environlMapper;
+
+    /**
+     * 查询环境记录列表
+     *
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public List<Environl> selectEnvironlList(Environl environl) {
+        return environlMapper.selectEnvironlList(environl);
+    }
+
+    /**
+     * 根据id批量删除数据
+     *
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int deleteEnvironlByid(String hid) {
+        return environlMapper.deleteEnvironlByid(hid);
+    }
+
+    /**
+     * 添加环境公告数据
+     *
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int insertEnvironl(Environl environl) {
+        return environlMapper.insertEnvironl(environl);
+    }
+
+    /**
+     * 根据hid选择数据
+     *
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public Environl selectByhid(String hid) {
+        return environlMapper.selectByhid(hid);
+    }
+
+    /**
+     * 删除环境信息
+     *
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int deleteEnvironlByids(String hid) {
+        return environlMapper.deleteEnvironlByids(hid);
+    }
+
+    /**
+     * 保存用户修改
+     *
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int updateEnvironl(Environl environl) {
+        return environlMapper.updateEnvironl(environl);
+    }
+}

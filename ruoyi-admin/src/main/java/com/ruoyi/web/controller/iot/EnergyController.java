2f20a482366f1804eca0abfd825b312e5d891992
Janeliao-C <1911679726@qq.com>
2019-12-31 22:00:06 +0800

    能源管理

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/iot/EnergyController.java
@@ -0,0 +1,112 @@
+package com.ruoyi.web.controller.iot;
+
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.base.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.page.TableDataInfo;
+import com.ruoyi.common.utils.ExcelUtil;
+import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.iot.domain.Energy;
+import com.ruoyi.iot.service.EnergyService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/iot/energy")
+public class EnergyController extends BaseController {
+
+    private String prefix = "iot/energy";
+
+    @Autowired
+    private EnergyService EnergyService;
+
+    @RequiresPermissions("iot:energyinfo:view")
+    @GetMapping()
+    public String Energy(){
+        return prefix+"/energy";
+    }
+
+    /**
+     * 导出终端运转列表
+     */
+    @Log(title = "Energy", businessType = BusinessType.EXPORT)
+    @RequiresPermissions("iot:energyinfo:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(Energy energy)
+    {
+        List<Energy> list = EnergyService.selectEnergyList(energy);
+        ExcelUtil<Energy> util = new ExcelUtil<Energy>(Energy.class);
+        return util.exportExcel(list, "energy");
+    }
+
+    /**
+     * Energy列表
+     * @param energy
+     * @return
+     */
+    //@RequiresPermissions("iot:energyinfo:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(Energy energy){
+        startPage();
+        List<Energy> list = EnergyService.selectEnergyList(energy);
+        return getDataTable(list);
+    }
+
+    @GetMapping("/add")
+    public String add(){
+        return prefix+"/add";
+    }
+
+    /**
+     * 新增Energy信息
+     */
+    @RequiresPermissions("iot:energyinfo:add")
+    @Log(title = "Energy信息", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(Energy energy)
+    {
+        return toAjax(EnergyService.insertEnergy(energy));
+    }
+    /**
+     * 修改Energy信息
+     */
+    @GetMapping("/edit/{eid}")
+    public String edit(@PathVariable("eid") String eid, ModelMap mmap)
+    {
+        Energy energy = EnergyService.selectByeid(eid);
+        mmap.put("energy", energy);
+        return prefix + "/edit";
+    }
+    /**
+     * 修改保存Energy信息
+     */
+    @RequiresPermissions("iot:energyinfo:edit")
+    @Log(title = "Energy信息", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(Energy energy)
+    {
+        return toAjax(EnergyService.updateEnergy(energy));
+    }
+    /**
+     * 删除Energy信息
+     */
+    @RequiresPermissions("iot:energyinfo:remove")
+    @Log(title = "删除Energy信息", businessType = BusinessType.DELETE)
+
+    @PostMapping( "/remove")
+    @ResponseBody
+    public AjaxResult remove( String ids)
+    {
+        //System.out.println("*******"+ids);
+        return toAjax(EnergyService.deleteEnergyByids(ids));
+    }
+}
---
+++ sx-iot/src/main/resources/java/com/ruoyi/iot/service/impl/EnergyServiceImpl.java
@@ -0,0 +1,96 @@
+package com.ruoyi.iot.service.impl;
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.enums.DataSourceType;
+import com.ruoyi.common.support.Convert;
+import com.ruoyi.iot.service.*;
+import com.ruoyi.iot.domain.Energy;
+import com.ruoyi.iot.mapper.EnergyMapper;
+import com.ruoyi.iot.service.EnergyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @program:  EnergySys
+ * @description: 能源管理系统
+ * @author: 饶泽敏
+ * @create: 2019-12-25 14:20
+ **/
+@Service
+public class EnergyServiceImpl implements EnergyService {
+    @Autowired
+    private EnergyMapper energyMapper;
+
+    /**
+     * 查询能源管理记录列表
+     *
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public List<Energy> selectEnergyList(Energy energy) {
+        return energyMapper.selectEnergyList(energy);
+    }
+
+    /**
+     * 根据id批量删除数据
+     * @param id
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int deleteEnergyByids(String eid) {
+        return energyMapper.deleteEnergyByids(Convert.toStrArray(eid));
+    }
+
+    /**
+     * 添加能源管理公告数据
+     * @param  Energy
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int insertEnergy(Energy energy) {
+        return energyMapper.insertEnergy(energy);
+    }
+
+    /**
+     * 根据eid选择数据
+     * @param eid
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public Energy selectByeid(String eid) {
+        return energyMapper.selectByid(eid);
+    }
+
+    /**@Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public Energy selectBybvo(String bvoltage) {
+        return energyMapper.selectBybvo(bvoltage);
+    }*/
+
+    /**
+     * 删除能源管理信息
+     * @param Energy
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int deleteEnergyByid(String eid) {
+        return energyMapper.deleteEnergyByid(eid);
+    }
+
+    /**
+     * 保存用户修改
+     * @param Energy
+     * @return
+     */
+    @Override
+    @DataSource(value = DataSourceType.SXINFOM)
+    public int updateEnergy(Energy energy) {
+        return energyMapper.updateEnergy(energy);
+    }
+
+}
